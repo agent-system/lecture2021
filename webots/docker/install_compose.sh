8deddd77730ce4ce5d1bbc4e202f9b258e2a1df4
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2021-04-14 19:48:52 +0900

    add install_compose.sh

---
+++ webots/docker/install_compose.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## install docker compose
+COMP_VER=$(docker-compose --version | sed -e 's@.*version \(.*\),.*@\1@')
+VER_MID=$(echo "${COMP_VER}" | sed -e 's@.*\.\([0-9]*\)\..*@\1@')
+
+if [ ${VER_MID} -lt ]; then
+   sudo apt remove docker-compose
+   LATEST_COMP_VER=$(wget https://api.github.com/repos/docker/compose/releases/latest -O - | grep 'tag_name' | cut -d\" -f4)
+   sudo wget https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -O /usr/local/bin/docker-compose
+fi
