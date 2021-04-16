33b8d3934bd57e31a0fe1a3c297f8ce0972401e7
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2021-04-16 23:25:12 +0900

    rebuild order of images

--- docker/build_gazebo.sh
+++ docker/build_gazebo.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-export BASE_IMAGE=jskrobotics/agent_system:ros_melodic_2021
+#export BASE_IMAGE=jskrobotics/agent_system:ros_melodic_2021
+#export BASE_IMAGE=jskrobotics/agent_system:robot_assembler_2021
+export BASE_IMAGE=jskrobotics/agent_system:webots_2021
 TEMP_IMAGE=temp_gazebo:latest
 ## output: jskrobotics/agent_system:gazebo11_dart_2021
 
@@ -14,39 +16,39 @@ if [ ! -e Dockerfile.gazebo_dart ]; then
     wget https://raw.githubusercontent.com/YoheiKakiuchi/misc_docker/build_gazebo/Dockerfile -O Dockerfile.gazebo_dart
 fi
 
-docker build . --no-cache -f Dockerfile.gazebo_dart --tag ${TEMP_IMAGE} --build-arg BASE_IMAGE=${BASE_IMAGE}
+docker build . --no-cache -f Dockerfile.gazebo_dart \
+       --build-arg BASE_IMAGE=${BASE_IMAGE} \
+       --build-arg ROS_SOURCE=/catkin_ws/install/setup.bash \
+       --tag jskrobotics/agent_system:gazebo11_dart_2021
 
-
-### wrap for xserver
-if [ -e gazebo_wrapper ]; then
-    rm -rf gazebo_wrapper
-fi
-git clone --depth=1 -b xserver_nvidia https://github.com/YoheiKakiuchi/misc_docker.git gazebo_wrapper
-
-cd gazebo_wrapper
-
-./wrap_for_docker_xserver.sh ${TEMP_IMAGE} --ros
-
-# fix docker-compose
-#sed -i -e "s@#command: // should be set at supervisord.conf@command: [bash, -c, 'until xset q; do sleep 3; done \&\& rosrun gazebo_ros gazebo ']@" docker-compose.yaml
-#cp docker-compose.yaml ../../docker-compose-gazebo.yaml
-## devert/xserver -> jskrobotics/agent_system:xserver_2021
-## ${TEMP_IMAGE}_xserver -> output
-## /userdir -> /jskrobotics
-
-# to build (dir)
-cd ..
-
-### tag for output image
-docker tag ${TEMP_IMAGE}_xserver jskrobotics/agent_system:gazebo11_dart_2021
-
-### for run/exec
-if [ ! -e ../run_gazebo.sh ]; then
-    wget https://github.com/YoheiKakiuchi/misc_docker/raw/master/run.sh  -O ../run_gazebo.sh
-fi
-if [ ! -e ../exec_gazebo.sh ]; then
-    wget https://github.com/YoheiKakiuchi/misc_docker/raw/master/exec.sh -O ../exec_gazebo.sh
-fi
+### not required because webots(robot_assembler) is already wrapped
+#> ### wrap for xserver
+#> if [ -e gazebo_wrapper ]; then
+#>     rm -rf gazebo_wrapper
+#> fi
+#> git clone --depth=1 -b xserver_nvidia https://github.com/YoheiKakiuchi/misc_docker.git gazebo_wrapper
+#> cd gazebo_wrapper
+#> ./wrap_for_docker_xserver.sh ${TEMP_IMAGE} --ros
+#> 
+#> # fix docker-compose
+#> #sed -i -e "s@#command: // should be set at supervisord.conf@command: [bash, -c, 'until xset q; do sleep 3; done \&\& rosrun gazebo_ros gazebo ']@" docker-compose.yaml
+#> #cp docker-compose.yaml ../../docker-compose-gazebo.yaml
+#> ## devert/xserver -> jskrobotics/agent_system:xserver_2021
+#> ## ${TEMP_IMAGE}_xserver -> output
+#> ## /userdir -> /jskrobotics
+#> 
+#> # to build (dir)
+#> cd ..
+#> ### tag for output image
+#> docker tag ${TEMP_IMAGE}_xserver jskrobotics/agent_system:gazebo11_dart_2021
+#> 
+#> ### for run/exec
+#> if [ ! -e ../run_gazebo.sh ]; then
+#>     wget https://github.com/YoheiKakiuchi/misc_docker/raw/master/run.sh  -O ../run_gazebo.sh
+#> fi
+#> if [ ! -e ../exec_gazebo.sh ]; then
+#>     wget https://github.com/YoheiKakiuchi/misc_docker/raw/master/exec.sh -O ../exec_gazebo.sh
+#> fi
 
 #docker image rm ${TEMP_IMAGE}
 #docker image rm ${TEMP_IMAGE}_xserver
