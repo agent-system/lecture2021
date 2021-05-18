83d1c333e8789d7265290df849a35211091da8a0
Masayuki Inaba <inaba@i.u-tokyo.ac.jp>
2021-05-19 01:58:09 +0900

    add kxr examples

---
+++ webots/projects/kxrl2l6a6h2/controllers/hello_world_demo/build/release/hello_world_demo.d
@@ -0,0 +1,6 @@
+build/release/hello_world_demo.o: hello_world_demo.c \
+ /home/inaba/webots/include/controller/c/webots/device.h \
+ /home/inaba/webots/include/controller/c/webots/nodes.h \
+ /home/inaba/webots/include/controller/c/webots/types.h \
+ /home/inaba/webots/include/controller/c/webots/motor.h \
+ /home/inaba/webots/include/controller/c/webots/robot.h
---
+++ webots/projects/salamander/controllers/salamander/build/release/salamander.d
@@ -0,0 +1,8 @@
+build/release/salamander.o: salamander.c \
+ /home/inaba/webots/include/controller/c/webots/distance_sensor.h \
+ /home/inaba/webots/include/controller/c/webots/types.h \
+ /home/inaba/webots/include/controller/c/webots/gps.h \
+ /home/inaba/webots/include/controller/c/webots/keyboard.h \
+ /home/inaba/webots/include/controller/c/webots/motor.h \
+ /home/inaba/webots/include/controller/c/webots/robot.h \
+ /home/inaba/webots/include/controller/c/webots/nodes.h
