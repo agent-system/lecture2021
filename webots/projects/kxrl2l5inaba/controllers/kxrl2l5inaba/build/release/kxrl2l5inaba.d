e571a5e445c6823b22ec259b8cad3550b4bda806
Masayuki Inaba <inaba@i.u-tokyo.ac.jp>
2021-05-26 12:07:35 +0900

    add files

---
+++ webots/projects/kxrl2l5inaba/controllers/kxrl2l5inaba/build/release/kxrl2l5inaba.d
@@ -0,0 +1,6 @@
+build/release/kxrl2l5inaba.o: kxrl2l5inaba.c \
+ /home/inaba/webots/include/controller/c/webots/device.h \
+ /home/inaba/webots/include/controller/c/webots/nodes.h \
+ /home/inaba/webots/include/controller/c/webots/types.h \
+ /home/inaba/webots/include/controller/c/webots/motor.h \
+ /home/inaba/webots/include/controller/c/webots/robot.h
