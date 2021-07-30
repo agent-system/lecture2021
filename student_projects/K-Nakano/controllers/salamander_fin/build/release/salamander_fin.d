4c8c7e7dc88749179f73b990ed5c515aed628b95
Kazashi-Nakano <a2aucjkf6@gmail.com>
2021-07-30 14:06:57 +0900

    nakano-work

---
+++ student_projects/K-Nakano/controllers/salamander_fin/build/release/salamander_fin.d
@@ -0,0 +1,11 @@
+build/release/salamander_fin.o: salamander_fin.c \
+ C:\Users\a2auc\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Users\a2auc\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\a2auc\AppData\Local\Programs\Webots/include/controller/c/webots/gps.h \
+ C:\Users\a2auc\AppData\Local\Programs\Webots/include/controller/c/webots/keyboard.h \
+ C:\Users\a2auc\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
+ C:\Users\a2auc\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\a2auc\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\a2auc\AppData\Local\Programs\Webots/include/controller/c/webots/camera.h \
+ C:\Users\a2auc\AppData\Local\Programs\Webots/include/controller/c/webots/camera_recognition_object.h \
+ C:\Users\a2auc\AppData\Local\Programs\Webots/include/controller/c/webots/gyro.h
