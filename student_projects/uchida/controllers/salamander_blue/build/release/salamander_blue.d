477ba4c54a38ca3baca1beb64abe87e37d101874
yaguchibinari <tomo1202uchida@gmail.com>
2021-08-02 16:56:28 +0900

    48216509_内田です。pull requestを送っているつもりなのですが、何か問題がありましたらtomo1202@g.ecc.u-tokyo.ac.jpまでご連絡ください。よろしくお願いいたします。

---
+++ student_projects/uchida/controllers/salamander_blue/build/release/salamander_blue.d
@@ -0,0 +1,9 @@
+build/release/salamander_blue.o: salamander_blue.c \
+ /home/mech-user/webots/include/controller/c/webots/distance_sensor.h \
+ /home/mech-user/webots/include/controller/c/webots/types.h \
+ /home/mech-user/webots/include/controller/c/webots/gps.h \
+ /home/mech-user/webots/include/controller/c/webots/keyboard.h \
+ /home/mech-user/webots/include/controller/c/webots/motor.h \
+ /home/mech-user/webots/include/controller/c/webots/robot.h \
+ /home/mech-user/webots/include/controller/c/webots/nodes.h \
+ /home/mech-user/webots/include/controller/c/webots/receiver.h
---
+++ student_projects/uchida/controllers/waterpolo_supervisor/build/release/waterpolo_supervisor.d
@@ -0,0 +1,6 @@
+build/release/waterpolo_supervisor.o: waterpolo_supervisor.c \
+ /home/mech-user/webots/include/controller/c/webots/emitter.h \
+ /home/mech-user/webots/include/controller/c/webots/types.h \
+ /home/mech-user/webots/include/controller/c/webots/robot.h \
+ /home/mech-user/webots/include/controller/c/webots/nodes.h \
+ /home/mech-user/webots/include/controller/c/webots/supervisor.h
